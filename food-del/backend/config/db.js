b62576411963ab684ad6d79cd71db1c83badfc9b
Nulemboon <tri2003714@gmail.com>
2024-04-08 21:08:00 +0700

    sample code

---
+++ food-del/backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://tri2003714:8825529tT@cluster0.tq9x0ib.mongodb.net//food-del').then(()=>console.log("DB Connected"))
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
