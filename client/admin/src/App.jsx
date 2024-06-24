b6340732007e67cd460e79f442c340856c6cdc37
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-06-24 21:26:47 +0700

    feat: staff

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
@@ -16,21 +16,28 @@ import Login from './pages/Login/Login';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem('authToken');
     setIsAuthenticated(!!token);
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    setIsAuthenticated(false);
+    navigate('/login');
+  };
+
   return (
     <div className='app'>
       <ToastContainer />
-      {isAuthenticated && <Navbar />}
+      {isAuthenticated && <Navbar onLogout={handleLogout} />}
       <hr />
       <div className="app-content">
         {isAuthenticated && <Sidebar />}
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
           {isAuthenticated ? (
             <>
               <Route path="/add" element={<Add />} />
@@ -40,6 +47,7 @@ const App = () => {
               <Route path="/vouchers" element={<Voucher />} />
               <Route path="/report" element={<FinancialReport />} />
               <Route path="/staff" element={<Staff />} />
+              <Route path="*" element={<Navigate to="/list" />} />
             </>
           ) : (
             <Route path="*" element={<Navigate to="/login" />} />
