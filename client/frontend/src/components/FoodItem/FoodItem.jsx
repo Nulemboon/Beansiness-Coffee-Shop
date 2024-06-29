a1a7ac9b9a6d0b85889c5977a34a1604d277442c
Mai Hoàng Đức <maihoangduc1103.ptts@gmail.com>
2024-06-29 11:19:36 +0700

    fix add to cart

--- client/frontend/src/components/FoodItem/FoodItem.jsx
+++ client/frontend/src/components/FoodItem/FoodItem.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import './FoodItem.css';
 import { assets } from '../../assets/assets';
 import { StoreContext } from '../../Context/StoreContext';
-import AddToCartModal from '../AddToCartModal/AddToCartModal'; 
+import AddToCartModal from '../AddToCartModal/AddToCartModal';
 import { useCookies } from 'react-cookie';
 import { toast } from 'react-toastify';
 
@@ -18,7 +18,7 @@ const FoodItem = ({ image, name, price, desc, id, toppings, onClick }) => {
     if (!token) {
       toast.error('Please sign in first to add items to your cart.', {
         position: 'top-right',
-        autoClose: 3000 
+        autoClose: 3000
       });
       return;
     }
@@ -38,7 +38,7 @@ const FoodItem = ({ image, name, price, desc, id, toppings, onClick }) => {
         key: productKey,
         _id: id,
         name: name,
-        price: totalPrice, 
+        price: totalPrice,
         quantity: quantity,
         size: size,
         toppings: selectedToppings,
@@ -53,7 +53,7 @@ const FoodItem = ({ image, name, price, desc, id, toppings, onClick }) => {
 
   const handleRemoveFromCartClick = (e) => {
     e.stopPropagation();
-    
+
     const currentCart = cookies.cart || [];
     const productKey = `${id}_${cartItems[id]?.size}_${cartItems[id]?.toppings?.join('_')}`;
 
@@ -63,13 +63,13 @@ const FoodItem = ({ image, name, price, desc, id, toppings, onClick }) => {
       if (currentCart[productIndex].quantity > 1) {
         currentCart[productIndex].quantity -= 1;
       } else {
-        currentCart.splice(productIndex, 1); 
+        currentCart.splice(productIndex, 1);
       }
     }
 
     setCookie('cart', currentCart, { path: '/' });
-    
-    setCartItems((prev) => {
+
+    setCartItems(prev => {
       const updatedCart = { ...prev };
       if (updatedCart[id]?.quantity > 1) {
         updatedCart[id].quantity -= 1;
@@ -84,38 +84,34 @@ const FoodItem = ({ image, name, price, desc, id, toppings, onClick }) => {
     <div className='food-item' onClick={onClick}>
       <div className='food-item-img-container'>
         <img className='food-item-image' src={`${url}/images/${image}`} alt={name} />
-        {/* {!cartItems[id] ? (
+        {!cartItems[id] ? (
           <img
             className='add'
-            onClick={handleAddToCartClick} 
+            onClick={handleAddToCartClick}
             src={assets.add_icon_white}
             alt="Add to cart"
           />
         ) : (
           <div className="food-item-counter">
             <img src={assets.remove_icon_red} onClick={handleRemoveFromCartClick} alt="Remove from cart" />
-            <p>{cartItems[id]?.quantity || 0}</p> 
+            <p>{cartItems[id]?.quantity || 0}</p>
             <img src={assets.add_icon_green} onClick={handleAddToCartClick} alt="Add more to cart" />
           </div>
-        )} */}
-        <div className="food-item-counter">
-            <img src={assets.remove_icon_red} onClick={handleRemoveFromCartClick} alt="Remove from cart" />
-            <p>{cartItems[id]?.quantity || 0}</p> 
-            <img src={assets.add_icon_green} onClick={handleAddToCartClick} alt="Add more to cart" />
-        </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p> <img src={assets.rating_starts} alt="Rating" />
         </div>
         <p className="food-item-desc">{desc}</p>
-        <p className="food-item-price">{price.toLocaleString('en-US', { style: 'currency', currency: 'VND' })}</p>      </div>
+        <p className="food-item-price">{price.toLocaleString('en-US', { style: 'currency', currency: 'VND' })}</p>
+      </div>
       <AddToCartModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
         onAddToCart={handleAddToCart}
         toppings={toppings}
-        price={price} 
+        price={price}
       />
     </div>
   );
